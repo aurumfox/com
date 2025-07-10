923282c91a3e91637d2e27c2641f2941e380b770
Fox <wnikolay28@yahoo.com>
2025-07-10 11:18:49 +0200

    Delete Db.js

--- Db.js
+++
@@ -1,9 +0,0 @@
-31 "@aws-sdk/client-kms": "^3.689.0",
-32 "@aws-sdk/client-secrets-manager": "^3.689.0",
-33 "@solana/web3.js": "^1.91.0",
-34 "awilix": "^10.0.1",
-
-
-
-  60 "@types/node": "^20.14.9",
-61 "eslint": "^8.57.0",
