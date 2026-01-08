d176435888290cae58f88214f6223d5e3b960d5b
Fox <wnikolay28@yahoo.com>
2026-01-08 04:50:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const SOL_DECIMALS = 9;
 const AFOX_DECIMALS = 6;
 const SECONDS_PER_DAY = 86400;
 const FIREBASE_PROXY_URL = 'https://firebasejs-key--snowy-cherry-0a92.wnikolay28.workers.dev/';
-const BACKUP_RPC_ENDPOINT = 'https://api.mainnet.solana.com'; // Попробуйте этот или возьмите бесплатный ключ на Helius.xyz
+const BACKUP_RPC_ENDPOINT = 'https://api.mainnet.solana.com'2ed0cb0f-85fc-410d-98da-59729966ec05';
 
 
 
