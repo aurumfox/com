03f3ee82c902b177dd04eb2846fa20720c5dc695
Fox <wnikolay28@yahoo.com>
2026-01-08 06:49:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ const SECONDS_PER_DAY = 86400;
 const FIREBASE_PROXY_URL = 'https://firebasejs-key--snowy-cherry-0a92.wnikolay28.workers.dev/';
 
 
-const BACKUP_RPC_ENDPOINT = 'https://api.mainnet-beta.solana.com/2ed0cb0f-85fc-410d-98da-59729966ec05';
+const BACKUP_RPC_ENDPOINT = 'https://api.mainnet-beta.solana.com';
+
+
 
 
 
