a526988102bb1ccfde5cfa453e1ea96d3399883e
aurumfox <wnikolay28@yahoo.com>
2026-06-25 10:56:36 +0200

    Update buffer.js

--- js/buffer.js
+++ js/buffer.js
@@ -1,9 +1,4 @@
-/*!
- * The buffer module from node.js, for the browser.
- *
- * @author   Feross Aboukhadijeh <https://feross.org>
- * @license  MIT
- */
+
 /* eslint-disable no-proto */
 
 'use strict'
